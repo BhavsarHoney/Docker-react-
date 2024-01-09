6f2a0cbca4ae0d2036f2424f66840ee3b3c5a9f3
Haniben Bhavsar <Haniben.Bhavsar@einfochips.com>
2024-01-09 12:40:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1> Hello  ....</h1>
+        <h2> This is Docker Practice  app....</h2>
+    </div>
+  );
+}
+
+export default App;
